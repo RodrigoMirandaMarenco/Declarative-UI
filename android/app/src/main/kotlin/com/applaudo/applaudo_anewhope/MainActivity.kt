afc3dcbc82fe64e4366e7dc2ad7cf2f2c53eb4a3
Pablo Reyes <devpab@gmail.com>
2020-09-16 14:30:55 -0600

    init project

---
+++ android/app/src/main/kotlin/com/applaudo/applaudo_anewhope/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.applaudo.applaudo_anewhope
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,118 @@
+import 'talk.dart';
+
+///
+/// Created by Pablo Reyes [preyes@applaudostudios.com]
+///
+
+final List<Talk> dataTalks = [
+  Talk(
+    title: 'Making your iOS App Accessible',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1035',
+    orator: Orator(
+      name: 'James Wankoviac',
+      picture:
+          'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-6.jpg',
+    ),
+  ),
+  Talk(
+    title: 'The sound of JavaScript',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1011',
+    orator: Orator(
+      name: 'Rita Walsh',
+      picture:
+          'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-8.jpg',
+    ),
+  ),
+  Talk(
+    title: 'The magic of Dependency Injection',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=100',
+    orator: Orator(
+      name: 'Diana Hill',
+      picture:
+          'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-5.jpg',
+    ),
+  ),
+  Talk(
+    title: 'Angular Testing',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1002',
+    orator: Orator(
+      name: 'Josh Mha',
+      picture:
+          'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-4.jpg',
+    ),
+  ),
+  Talk(
+    title: 'Laravel - OAuth',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1018',
+    orator: Orator(
+      name: 'Kiara Utz',
+      picture:
+          'https://images.generated.photos/c1z4QKwP-KrvZ1mQYGVSciFubvLD7S12cNlGsvemVk0/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA5MTk5NjguanBn.jpg',
+    ),
+  ),
+  Talk(
+    title: 'Nest JS: The Big Picture',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1033',
+    orator: Orator(
+      name: 'Hector Fuentes',
+      picture:
+          'https://images.generated.photos/lZtzErbFWitHp8X44Uv14GOIqENA_aqEQVhAgaNTOBE/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA1MTA4NTkuanBn.jpg',
+    ),
+  ),
+  Talk(
+    title: 'Azure PAAS Services',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1040',
+    orator: Orator(
+      name: 'Andrea Barrientos',
+      picture:
+          'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-7.jpg',
+    ),
+  ),
+  Talk(
+    title: 'AWS Serverless CI/CD',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1047',
+    orator: Orator(
+      name: 'Mike Mars',
+      picture:
+          'https://images.generated.photos/pmb8-ABvccZAWpxEjghXrXGM7WlfUPS5zEzh_0xJA88/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzA0NzMxODYuanBn.jpg',
+    ),
+  ),
+  Talk(
+    title: 'Infrastructure as Code and how you can use Terraform',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=1043',
+    orator: Orator(
+      name: 'Claudia Gluz',
+      picture:
+          'https://images.generated.photos/sJmfexksXWxFw3lTbNsW4fPSIyalcbmwBTn-dqGnTJ4/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAyNTYzNzEuanBn.jpg',
+    ),
+  ),
+  Talk(
+    title: 'Building With Bricks',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris risus urna, fermentum et aliquet vitae, tincidunt et felis. Cras semper eros non lorem hendrerit maximus. Suspendisse et laoreet quam.',
+    picture: 'https://picsum.photos/500/300?image=129',
+    orator: Orator(
+      name: 'Jose Dufa',
+      picture:
+          'https://images.generated.photos/niVbgCeJSct6_VRbOFnVc4sHvz6_4TzWbNVy3h3BFzE/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAxNTA4NDYuanBn.jpg',
+    ),
+  ),
+];
