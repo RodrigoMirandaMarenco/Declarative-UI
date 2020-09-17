6867189d968f0b1162cca89006f51d4ccb3c49b8
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 14:37:12 -0600

    🎨 Change theme colors

--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/util/Color.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/util/Color.kt
@@ -2,7 +2,7 @@ package com.rmiranda.androidjetpackcompose.ui.util
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+val green200 = Color(0xFF80E27E)
+val green500 = Color(0xFF4CAF50)
+val green700 = Color(0xFF087F23)
--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/util/Theme.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/util/Theme.kt
@@ -7,19 +7,22 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-        primary = purple200,
-        primaryVariant = purple700,
-        secondary = teal200
+    primary = green200,
+    primaryVariant = green700,
+    secondary = teal200
 )
 
 private val LightColorPalette = lightColors(
-        primary = purple500,
-        primaryVariant = purple700,
-        secondary = teal200
+    primary = green500,
+    primaryVariant = green700,
+    secondary = teal200
 )
 
 @Composable
-fun AndroidJetpackComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun AndroidJetpackComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
@@ -27,9 +30,9 @@ fun AndroidJetpackComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), conte
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = typography,
-            shapes = shapes,
-            content = content
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
     )
 }
