be8a19d035e49ac349549f4c8ac133d97b75391d
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 11:40:16 -0600

    ✨ Add basic composables structure

--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
@@ -2,13 +2,20 @@ package com.rmiranda.androidjetpackcompose.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.rmiranda.androidjetpackcompose.R
+import com.rmiranda.androidjetpackcompose.data.model.Talk
 import com.rmiranda.androidjetpackcompose.ui.util.AndroidJetpackComposeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    CustomTopAppBar(getString(R.string.app_name))
+                    MainScreen(getString(R.string.app_name), Talk.dummyTalks())
                 }
             }
         }
@@ -26,10 +32,40 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
+fun MainScreen(customTitle: String, talks: List<Talk>) {
+    Column {
+        CustomTopAppBar(customTitle = customTitle)
+        TalkList(talks = talks)
+    }
+}
+
+@Composable
 fun CustomTopAppBar(customTitle: String) {
     TopAppBar(
-            title = {
-                Text(text = customTitle)
-            }
+        title = {
+            Text(text = customTitle)
+        }
     )
+}
+
+@Composable
+fun TalkList(talks: List<Talk>) {
+    ScrollableColumn {
+        talks.forEach {
+            TalkItem(talk = it)
+        }
+    }
+}
+
+@Composable
+fun TalkItem(talk: Talk) {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = talk.title
+            )
+        }
+    }
 }
