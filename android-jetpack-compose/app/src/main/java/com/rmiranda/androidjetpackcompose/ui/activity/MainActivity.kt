aa9eff972f3ae6b030de222c3d3da78aa1d22d52
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 11:05:42 -0600

    ✨ Add Custom TopAppBar composable

--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.rmiranda.androidjetpackcompose.ui.activity
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.ColumnScope.gravity
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
+import com.rmiranda.androidjetpackcompose.R
 import com.rmiranda.androidjetpackcompose.ui.util.AndroidJetpackComposeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             AndroidJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    CustomTopAppBar(getString(R.string.app_name))
                 }
             }
         }
@@ -25,14 +34,10 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidJetpackComposeTheme {
-        Greeting("Android")
-    }
+fun CustomTopAppBar(customTitle: String) {
+    TopAppBar(
+            title = {
+                Text(text = customTitle)
+            }
+    )
 }
