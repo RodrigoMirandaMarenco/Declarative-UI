e803a284b33f5b26ef1469b464acadd34d6945a2
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 11:06:43 -0600

    🎨 Remove unused code

--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.rmiranda.androidjetpackcompose.ui.activity
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.ColumnScope.gravity
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
 import com.rmiranda.androidjetpackcompose.R
 import com.rmiranda.androidjetpackcompose.ui.util.AndroidJetpackComposeTheme
 
