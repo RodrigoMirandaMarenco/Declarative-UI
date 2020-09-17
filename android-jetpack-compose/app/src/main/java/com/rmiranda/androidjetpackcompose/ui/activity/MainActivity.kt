29011c0dfa2132fa02357c627f8e052d7153373a
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 14:14:35 -0600

    ✨ Add the rest of the UI elements

--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
@@ -2,10 +2,12 @@ package com.rmiranda.androidjetpackcompose.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Box
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
@@ -15,9 +17,11 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import com.koduok.compose.glideimage.GlideImage
 import com.rmiranda.androidjetpackcompose.R
 import com.rmiranda.androidjetpackcompose.data.model.Talk
 import com.rmiranda.androidjetpackcompose.ui.util.AndroidJetpackComposeTheme
+import com.rmiranda.androidjetpackcompose.ui.util.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,24 +55,62 @@ fun CustomTopAppBar(customTitle: String) {
 
 @Composable
 fun TalkList(talks: List<Talk>) {
-    ScrollableColumn {
+    ScrollableColumn(modifier = Modifier.padding(horizontal = 16.dp)) {
+        CustomSpacer()
         talks.forEach {
-            TalkItem(talk = it)
+            ItemCard(talk = it)
+            CustomSpacer()
+        }
+    }
+}
+
+@Composable
+fun ItemCard(talk: Talk) {
+    MaterialTheme {
+        Card(
+            shape = RoundedCornerShape(8.dp),
+            backgroundColor = MaterialTheme.colors.surface
+        ) {
+            Box {
+                TalkItem(talk = talk)
+            }
         }
     }
 }
 
 @Composable
 fun TalkItem(talk: Talk) {
-    MaterialTheme {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
+    GlideImage(talk.pictureUrl) {
+        centerCrop()
+    }
+    Row(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Column(modifier = Modifier.width(56.dp).padding(8.dp)) {
+            GlideImage(model = talk.speaker.pictureUrl) {
+                circleCrop()
+            }
+        }
+        Column {
             Text(
-                text = talk.title
+                text = talk.title,
+                style = typography.h6,
+            )
+            Text(
+                text = talk.speaker.name,
+                style = typography.subtitle1,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            Text(
+                text = talk.description
             )
         }
     }
+}
+
+@Composable
+fun CustomSpacer() {
+    Spacer(modifier = Modifier.preferredHeight(16.dp))
 }
 
 @Preview
