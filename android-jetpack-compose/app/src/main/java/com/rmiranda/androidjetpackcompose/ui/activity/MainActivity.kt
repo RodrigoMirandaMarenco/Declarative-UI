8303c06a3646436b8a3d7ae3be03ccf3bf6c3093
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 11:41:34 -0600

    ✨ Add PreviewTalkItem

--- android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/ui/activity/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 import com.rmiranda.androidjetpackcompose.R
 import com.rmiranda.androidjetpackcompose.data.model.Talk
 import com.rmiranda.androidjetpackcompose.ui.util.AndroidJetpackComposeTheme
@@ -68,4 +69,10 @@ fun TalkItem(talk: Talk) {
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewTalkItem() {
+    TalkItem(talk = Talk.dummyTalks().first())
 }
