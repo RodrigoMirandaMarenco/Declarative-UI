a5844a9f0553fa082a09b8d66fee8cf3bd730620
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-09-17 09:59:59 -0600

    ✨ Add Talk data class

---
+++ android-jetpack-compose/app/src/main/java/com/rmiranda/androidjetpackcompose/data/model/Talk.kt
@@ -0,0 +1,8 @@
+package com.rmiranda.androidjetpackcompose.data.model
+
+data class Talk(
+        val title: String,
+        val description: String,
+        val pictureUrl: String,
+        val speaker: Speaker
+)
